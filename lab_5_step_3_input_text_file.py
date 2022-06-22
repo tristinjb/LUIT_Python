a0504211a9f1ca45e9bf5198a294903172a80e94
Tristin Bringman <tristinjb98@hotmail.com>
2022-06-22 23:43:24 +0000

    Inputs lab 5

---
+++ lab_5_step_3_input_text_file.py
@@ -0,0 +1,12 @@
+with open(filename, 'r' ) as variable_name:
+    <Do something with the variable here>
+def open_input(file):
+    with open(file, 'r') as f:
+        text = f.read() #We use read() to read the actual contents of the file
+        print(text)
+
+def main():
+    open_input("text.txt")
+
+if __name__=="__main__":
+    main()
