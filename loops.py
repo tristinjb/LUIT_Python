9b0c4839c229a8d3253cddc6443ec416bc280870
Tristin Bringman <tristinjb98@hotmail.com>
2022-06-23 00:06:33 +0000

    loops lab 6

---
+++ loops.py
@@ -0,0 +1,14 @@
+fruit = ['apples','oranges','bananas']
+
+for item in fruit:
+    print(f'The best fruit now is {item}')
+
+numbers = [0,1,2,3,4,5,6,7,8,9,10]
+for number in numbers:
+     print(f'The next number is {number}')
+
+for number in range(1,10):
+     print(f'The next number is {number}')
+
+for number in range(1,10,2):
+     print(f'The next number is {number}')
